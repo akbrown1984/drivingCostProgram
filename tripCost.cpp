86cc9decbd9bb73215e0852d9c809422181827ea
akbrown1984 <akbrown1984@gmail.com>
2025-08-14 19:54:14 -0400

    Update tripCost.cpp
    added comments

--- tripCost.cpp
+++ tripCost.cpp
@@ -1,3 +1,26 @@
+// Function DrivingCost calculates trip cost 
+// drivenMiles: distance
+// milesPerGallon: efficiency
+// dollarsPerGallon: fuel price
+Function DrivingCost(float drivenMiles, float milesPerGallon, float dollarsPerGallon) returns float costMiles
+   costMiles = drivenMiles * (1.0 / milesPerGallon) * dollarsPerGallon
+   return costMiles
+
+// Main function reads input and outputs trip costs
+Function Main() returns nothing
+   float milesPerGallon
+   float dollarsPerGallon
+
+   milesPerGallon = Get next input
+   dollarsPerGallon = Get next input
+
+   Put DrivingCost(10.0, milesPerGallon, dollarsPerGallon) to output with 2 decimal places
+   Put " " to output
+   Put DrivingCost(50.0, milesPerGallon, dollarsPerGallon) to output with 2 decimal places
+   Put " " to output
+   Put DrivingCost(400.0, milesPerGallon, dollarsPerGallon) to output with 2 decimal places
+
+
 Function DrivingCost(float drivenMiles, float milesPerGallon, float dollarsPerGallon) returns float costMiles
    costMiles = drivenMiles * (1.0 / milesPerGallon) * dollarsPerGallon
    return costMiles
